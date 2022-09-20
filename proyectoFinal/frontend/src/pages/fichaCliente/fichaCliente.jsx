70a44fc8bcd1e80cdd74a6e647aaf22d011ae51e
Marcos <marcoscba5@gmail.com>
2022-09-19 21:05:55 -0300

    fix colums cliente

--- proyectoFinal/frontend/src/pages/fichaCliente/fichaCliente.jsx
+++ proyectoFinal/frontend/src/pages/fichaCliente/fichaCliente.jsx
@@ -99,7 +99,9 @@ export default function Servicios() {
     { field: "tipo_cabello", headerName: "Tipo de Cabello", width: 100 },
     { field: "estado_cabello", headerName: "Estado del Cabello", width: 100 },
     { field: "formula", headerName: "Formula", width: 300 },
-    { field: "clientes.nombre", headerName: "Cliente", width: 100 },
+    { field: "cliente.nombre", headerName: "Cliente", width: 130 , renderCell: (params) => (
+          <p>{params.row.cliente.nombre}</p>
+          )},
     {
       field: "acciones",
       headerName: "Acciones",
