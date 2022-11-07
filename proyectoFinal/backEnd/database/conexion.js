12b0596fb4fe665f77a680272a3f731c7503b8c2
Marcos Iscovich <miscovich@na-sa.com.ar>
2022-11-07 14:56:16 -0300

    modificaciones power

--- proyectoFinal/backEnd/database/conexion.js
+++ proyectoFinal/backEnd/database/conexion.js
@@ -2,7 +2,7 @@ module.exports = {
     host: 'localhost',
     dialect: 'mysql',
     username: 'root',
-    password: 'admin',
-    database: 'imagenbd',
+    password: 'root', /* admin */
+    database: 'database1', /* imagenbd */
     port:'3306'
 }
