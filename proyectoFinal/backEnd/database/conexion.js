747d2cc875be529b48a7a18055963f0ed7955bda
Marcos Iscovich <miscovich@na-sa.com.ar>
2022-11-08 08:16:21 -0300

    modificaciones en database conexion

--- proyectoFinal/backEnd/database/conexion.js
+++ proyectoFinal/backEnd/database/conexion.js
@@ -2,7 +2,7 @@ module.exports = {
     host: 'localhost',
     dialect: 'mysql',
     username: 'root',
-    password: 'root', /* admin */
-    database: 'database1', /* imagenbd */
+    password: 'root', /* admin *//*  root */
+    database: 'database1', /* imagenbd */ /* database1 */
     port:'3306'
 }
